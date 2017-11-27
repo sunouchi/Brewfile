6a0c60488f2188710f905b0f0c3307844a5379c6
sunouchi <t@sunouchi.jp>
2017-11-28 00:53:42 +0900

    Make brew-install.sh

---
+++ brew-install.sh
@@ -0,0 +1,118 @@
+## homebrew
+#tap 'caskroom/cask'
+#tap 'homebrew/bundle'
+#tap 'homebrew/versions'
+#tap 'homebrew/core'
+#tap 'homebrew/python'
+#tap 'homebrew/science'
+#tap 'rcmdnk/file'
+#tap 'argon/mas'
+#brew 'argon/mas/mas'
+#mas 'installed apps found', id: No
+
+# editor
+brew install lua
+brew install tmux
+brew install vim --with-lua
+brew install ctags
+brew install zsh
+
+# ruby
+brew install ruby-build
+brew install rbenv
+brew install imagemagick
+brew install libevent
+brew install openssl
+brew install readline
+
+# javascript
+brew install node
+
+# python
+#brew 'python'
+brew install python3
+
+# data analysis
+brew cask install aquaterm
+brew cask install cuda
+brew cask install java
+brew cask install xquartz
+brew install gnuplot --with-aquaterm --with-x11
+brew install homebrew/python/numpy
+brew install homebrew/science/veclibfort
+brew install homebrew/science/arpack
+brew install homebrew/science/glpk
+brew install homebrew/science/hdf5
+brew install homebrew/science/qhull
+brew install homebrew/science/qrupdate
+brew install homebrew/science/octave
+brew install homebrew/science/opencv
+brew install homebrew/science/suite-sparse421
+
+# utilities
+brew install coreutils
+brew install nkf
+brew install osxutils
+brew install pwgen
+brew install tree
+brew install wget
+cask install virtualbox
+
+# database
+brew install mongodb
+brew install mysql
+brew install sqlite
+
+# development tools
+#brew cask install google-chrome
+#brew cask install firefox
+brew cask install vagrant
+#brew cask install slack
+#brew cask install atom
+brew cask install filezilla
+
+# utility mac apps
+#brew cask install bettertouchtool
+#brew cask install fluid
+#brew cask install licecap
+#brew cask install google-japanese-ime
+
+# others
+brew install autoconf
+brew install xz
+brew install automake
+brew install bazel
+brew install pkg-config
+brew install gmp
+brew install isl
+brew install cloog
+brew install cmake
+brew install doxygen
+brew install eigen
+brew install jpeg
+brew install libtiff
+brew install little-cms2
+brew install ghostscript
+brew install epstool
+brew install mpfr
+brew install libmpc
+brew install gcc
+brew install fftw
+brew install libpng
+brew install freetype
+brew install fontconfig
+brew install libtool
+brew install gd
+brew install gdbm
+brew install gl2ps
+brew install graphicsmagick
+brew install ilmbase
+brew install openexr
+brew install libusb --universal
+brew install libfreenect
+brew install pcre
+brew install plotutils
+brew install pstoedit
+brew install swig
+brew install szip
+brew install tbb
